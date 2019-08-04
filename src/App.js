313047b78dc0ee9b44d44f29b50123053b8eafcc
翁治平 <gocreating@gmail.com>
2019-08-04 23:08:21 +0800

    Import component style to test functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import './App.css';
 
 const App = () => (
   <Switch>
