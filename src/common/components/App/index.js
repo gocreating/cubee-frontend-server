c40a09a0b50d35cba864b9415c7a3c49e99f33c3
翁治平 <gocreating@gmail.com>
2019-08-25 02:33:13 +0800

    Update global style

--- src/common/components/App/index.js
+++ src/common/components/App/index.js
@@ -5,9 +5,59 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import { Helmet } from 'react-helmet';
 import './App.css';
 
+/* ref: <https://alligator.io/css/minimal-css-reset/> */
+const ResetStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+`;
+
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Lato|Noto+Sans+TC&display=swap&subset=chinese-traditional');
+
+  body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+    font-family: ${props => props.theme.type.fontFamily};
+  }
   body {
-    background-color: #eee;
+    background-color: ${props => props.theme.colors.white};
+  }
+  h1 {
+    font-size: ${props => props.theme.type.fontSizeH1};
+  }
+  h2 {
+    font-size: ${props => props.theme.type.fontSizeH2};
+  }
+  h3 {
+    font-size: ${props => props.theme.type.fontSizeH3};
+  }
+  h4 {
+    font-size: ${props => props.theme.type.fontSizeH4};
+  }
+  h5 {
+    font-size: ${props => props.theme.type.fontSizeH5};
+  }
+  h6 {
+    font-size: ${props => props.theme.type.fontSizeH6};
   }
 `;
 
@@ -62,6 +112,7 @@ const App = () => (
         <title>Cubee</title>
         <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       </Helmet>
+      <ResetStyle />
       <GlobalStyle />
       <Switch>
         <Route exact path="/" component={HomePage} />
