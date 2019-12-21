32efa68658e809230d5317a77a8e66b0fc8899f4
翁治平 <gocreating@gmail.com>
2019-12-22 01:57:20 +0800

    Add login page

---
+++ src/common/pages/LoginPage/index.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import { withLayout } from '../../layouts/AppLayout';
+import Container from '../../components/Container';
+import Heading from '../../components/Heading';
+import Form from '../../components/Form';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import {
+  loginRequest,
+  selectors as authSelectors,
+} from '../../ducks/auth';
+
+class LoginPage extends Component {
+  usernameRef = React.createRef();
+  passwordRef = React.createRef();
+
+  handleBtnLoginClick = () => {
+    const { loginRequest } = this.props;
+    loginRequest(this.usernameRef.current.value, this.passwordRef.current.value);
+  }
+
+  render() {
+    const { isLoading } = this.props;
+
+    return (
+      <Container>
+        <Helmet>
+          <title>Login to Cubee</title>
+        </Helmet>
+        <Heading level={2}>Login to Cubee</Heading>
+        <Form>
+          <Form.Field>
+            <label htmlFor="username">Username</label>
+            <Input
+              ref={this.usernameRef}
+              id="username"
+              type="text"
+              autoComplete="new-password"
+            />
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor="password">Password</label>
+            <Input
+              ref={this.passwordRef}
+              id="password"
+              type="password"
+              autoComplete="new-password"
+            />
+          </Form.Field>
+          <Button
+            type="button"
+            variant="primary"
+            disabled={isLoading}
+            onClick={this.handleBtnLoginClick}
+          >
+            Login
+          </Button>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+LoginPage.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  loginRequest: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  isLoading: authSelectors.getIsLoading(state),
+});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  loginRequest,
+}, dispatch);
+
+export default withLayout({ nav: true })(
+  connect(mapStateToProps, mapDispatchToProps)(
+    LoginPage,
+  ),
+);
