312d0044da141c69aa9a8bc34fe1357bf4920c3b
翁治平 <gocreating@gmail.com>
2019-12-22 01:55:38 +0800

    Update api agent

--- src/common/api/agent.js
+++ src/common/api/agent.js
@@ -1,8 +1,5 @@
 import fetch from 'cross-fetch';
 
 export default (...args) => fetch(...args).then(res => {
-  if (res.status >= 400) {
-    throw new Error('Bad response from server');
-  }
   return res.json();
 });
