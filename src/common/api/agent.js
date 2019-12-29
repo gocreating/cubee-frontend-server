e3fe3e26827f6bc1d68245ff341115782dbae39b
翁治平 <gocreating@gmail.com>
2019-12-29 18:09:26 +0800

    Fix api agent

--- src/common/api/agent.js
+++ src/common/api/agent.js
@@ -1,4 +1,15 @@
 import fetch from 'cross-fetch';
+import { fromJS } from 'immutable';
+
+export const injectCredentials = (fetchOptions = {}, accessToken) => {
+  if (accessToken) {
+    return fromJS(fetchOptions)
+      .setIn(['headers', 'Authorization'], `Bearer ${accessToken}`)
+      .toJS();
+  } else {
+    return fetchOptions;
+  }
+};
 
 export default (...args) => fetch(...args).then(res => {
   return res.json();
