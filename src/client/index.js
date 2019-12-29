c638823741fed62db538eb1425e8cfde75d891c1
翁治平 <gocreating@gmail.com>
2019-12-29 18:39:38 +0800

    Setup redux persist

--- src/client/index.js
+++ src/client/index.js
@@ -7,7 +7,7 @@ import configureStore from '../common/store/configureStore';
 import App from '../common/components/App';
 
 const preloadedState = window.__PRELOADED_STATE__;
-const store = configureStore(preloadedState);
+const { store } = configureStore(preloadedState);
 
 delete window.__PRELOADED_STATE__;
 
--- src/server/middlewares/configureStore.js
+++ src/server/middlewares/configureStore.js
@@ -2,7 +2,7 @@ import configureStore from '../../common/store/configureStore';
 
 const configureStoreMiddleware = (req, res, next) => {
   const initialState = {};
-  const store = configureStore(initialState);
+  const { store } = configureStore(initialState);
   res.locals.store = store;
   next();
 };
