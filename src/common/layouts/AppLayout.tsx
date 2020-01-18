f0ec9b6625f5686a4cce2425dce50bbd3e5dda94
翁治平 <gocreating@gmail.com>
2020-01-19 00:31:30 +0800

    Migrate common/layouts/AppLayout to tsx

---
+++ src/common/layouts/AppLayout.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Nav from './Nav';
+
+interface Props {
+  nav?: boolean;
+  children?: React.ReactNode;
+}
+
+const AppLayout: React.FunctionComponent<Props> = ({ nav, children }) => (
+  <>
+    {nav && <Nav />}
+    {children}
+  </>
+);
+
+AppLayout.propTypes = {
+  nav: PropTypes.bool,
+  children: PropTypes.node,
+};
+
+AppLayout.defaultProps = {
+  nav: false,
+  children: null,
+};
+
+export const withLayout = <BaseProps extends {}>(layoutProps: Props) => (BaseComponent: React.ComponentType<BaseProps>) => (pageProps: BaseProps) => (
+  <AppLayout {...layoutProps}>
+    <BaseComponent {...pageProps} />
+  </AppLayout>
+);
+
+export default AppLayout;
