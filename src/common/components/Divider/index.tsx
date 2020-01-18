bd3749b5510e7cda0ca53c64b2f59a65a960b912
翁治平 <gocreating@gmail.com>
2020-01-19 00:04:32 +0800

    Refine Divider types

--- src/common/components/Divider/index.tsx
+++ src/common/components/Divider/index.tsx
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 import { variant } from 'styled-system';
 import themeGet from '@styled-system/theme-get';
-import Box from '../Box';
+import Box, { Props as BoxProps } from '../Box';
 
-interface Props {
+interface Props extends BoxProps{
   variant: 'horizontal' | 'vertical';
 }
 
