77e59f27a544be093c2ac6b0b9a32e0dd8077c68
翁治平 <gocreating@gmail.com>
2020-01-23 22:31:42 +0800

    Add post duck

--- src/common/sagas/index.ts
+++ src/common/sagas/index.ts
@@ -1,8 +1,10 @@
 import { rootSaga as authSagas } from '../ducks/auth';
+import { rootSaga as postSagas } from '../ducks/post';
 import { rootSaga as statusSagas } from '../ducks/status';
 
 const sagas = {
   auth: authSagas,
+  post: postSagas,
   status: statusSagas,
 };
 
