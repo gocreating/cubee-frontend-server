94643cf1a75b02ac5c62219663fc32db1eff0fd8
翁治平 <gocreating@gmail.com>
2020-01-23 15:26:38 +0800

    Refine env

--- src/common/utils/env.ts
+++ src/common/utils/env.ts
@@ -2,22 +2,25 @@ import { ApplicationEnvironment } from 'cubee';
 
 const isServer = (typeof window === 'undefined');
 const isBrowser = !isServer;
+
+const isStaging = (
+  isBrowser
+    ? (window.CUBEE_ENV === 'staging')
+    : (process.env.CUBEE_ENV === 'staging')
+);
+const isTesting = (process.env.NODE_ENV === 'test');
+const isProduction = (
+  isBrowser
+    ? (window.CUBEE_ENV === 'production')
+    : (process.env.CUBEE_ENV === 'production')
+);
+
 const env: ApplicationEnvironment = {
   isServer,
   isBrowser,
-  isStaging: (
-    isBrowser
-      ? (window.CUBEE_ENV === 'staging')
-      : (process.env.CUBEE_ENV === 'staging')
-  ),
-  isTesting: (
-    process.env.NODE_ENV === 'test'
-  ),
-  isProduction: (
-    isBrowser
-      ? (window.CUBEE_ENV === 'production')
-      : (process.env.CUBEE_ENV === 'production')
-  ),
+  isStaging,
+  isTesting,
+  isProduction,
 };
 
 export default env;
