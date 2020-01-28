d21df44bb7c35e00614d33bf7c197601a2a346f1
翁治平 <gocreating@gmail.com>
2020-01-28 16:43:32 +0800

    Integrate `handleRequestFail` with message duck

--- src/common/utils/sagaUtils.ts
+++ src/common/utils/sagaUtils.ts
@@ -1,16 +1,21 @@
 import { ApiFailActionPayload } from 'cubee';
 import { fromJS } from 'immutable';
+import { put } from 'redux-saga/effects';
+import { MessageType, pushMessage } from '../ducks/message';
 
 interface RequestFailAction {
   type: string;
   payload: ApiFailActionPayload;
 }
 
-export const handleRequestFail = (action: RequestFailAction) => {
+export const handleRequestFail = function* (action: RequestFailAction) {
   const { res, error } = action.payload;
+  let message = '';
   if (res) {
-    alert(fromJS(res).getIn(['data', 'message'], 'Some remote error happened.'));
+    message = fromJS(res)
+      .getIn(['data', 'message'], 'Some remote error happened.');
   } else if (error) {
-    alert('Some local error happened.');
+    message = 'Some local error happened.';
   }
+  yield put(pushMessage(MessageType.ERROR, message));
 }
